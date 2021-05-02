b3e31eb6790ec5c8ec9d332cdf64f87fdefa6e0b
Albanec671 <lk5117@ic.ac.uk>
2021-05-02 20:32:28 +0100

    Added one more configuration to Global.py for comparison with the parameters used in one of the first RL papers

--- Global.py
+++ Global.py
@@ -4,9 +4,20 @@ import numpy as np
 class Config:
     LOG_ENABLED = True
     MAX_DELAY_BEFORE_SLEEP_MS = 500
-    SIMULATION_TIME = 1000*60*60*24*2
+    SIMULATION_TIME = 1000*60*60*24*10
     PRINT_ENABLED = False
-    CELL_SIZE = 500
+    CELL_SIZE = 1000
     MAX_DELAY_START_PER_NODE_MS = np.round(SIMULATION_TIME / 10)
-    num_nodes = 10
+    num_nodes = 1000
     track_changes = True
+
+class Paper1:
+    LOG_ENABLED = True
+    MAX_DELAY_BEFORE_SLEEP_MS = 500
+    SIMULATION_TIME = 1000 * 60 * 60 * 150
+    PRINT_ENABLED = False
+    CELL_SIZE = 1500
+    MAX_DELAY_START_PER_NODE_MS = np.round(SIMULATION_TIME / 10)
+    num_nodes = 30
+    track_changes = True
+

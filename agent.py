7c697c3fdaab8a75be2a1dd3bd4e7714bcc3769c
Leon <leonkhalmetov@gmail.com>
2021-05-05 20:31:06 +0100

    Improved experience replay buffer, now network is called on the whole batch, no a for loop; implemented both for Q-learning and SARSA; also added weight normalization for the network Linear Layers;

--- agent.py
+++ agent.py
@@ -5,6 +5,7 @@ import numpy as np
 import math
 import random
 import torch
+import torch.nn.utils.weight_norm as weight_norm
 
 # Deep Reinforcement Learning Agent section
 class LearningAgent:
@@ -126,11 +127,12 @@ class LearningAgent:
 # Deep Reinforcement Learning Agent section
 class DeepLearningAgent:
 
-    def __init__(self, env, depth, state_space_dimensions, lr=0.001, gamma=0.1, epsilon=0.5, sarsa=False):
+    def __init__(self, env, depth, state_space_dimensions, lr=0.001, gamma=0.1, epsilon=0.5, sarsa=False, replay_buffer=False):
 
         self.type = "Deep Q Learning"
         self.nodes = []
         self.sarsa = sarsa
+
         # Create a Q-network, which predicts the q-value for a particular state.
         self.env = env
         self.q_network = Network(input_dimensions=state_space_dimensions, output_dimensions=90, depth=depth)
@@ -154,6 +156,7 @@ class DeepLearningAgent:
 
         self.losses = {}
 
+        self.replay_buffer = replay_buffer
         self.buffer = ReplayBuffer(50)
         self.batch_size = 10
         self.lr = lr
@@ -202,16 +205,20 @@ class DeepLearningAgent:
         # Set all the gradients stored in the optimiser to zero.
         self.optimiser.zero_grad()
 
-        # Calculate the loss for this transition.
-        loss = self._calculate_loss(transition)
+        if not self.replay_buffer:
 
-        # self.buffer.push(transition)
-        #
-        # if self.buffer.sizeof() < self.batch_size:
-        #     return
-        #
-        # transitions = self.buffer.sample(self.batch_size)
-        # loss = self._calculate_batch_loss(transitions)
+            # Calculate the loss for this transition.
+            loss = self._calculate_loss(transition)
+
+        else:
+            # Implement replay buffer
+            self.buffer.push(transition)
+
+            if self.buffer.sizeof() < self.batch_size:
+                return
+
+            transitions = self.buffer.sample(self.batch_size)
+            loss = self._calculate_batch_loss(transitions)
 
         # Compute the gradients based on this loss, i.e. the gradients of the loss with respect to the Q-network parameters.
         loss.backward()
@@ -222,28 +229,74 @@ class DeepLearningAgent:
         # Return the loss as a scalar
         return loss.item()
 
+    # def _calculate_batch_loss(self, transitions):
+    #     transposed = list(zip(*transitions))
+    #
+    #     states = [s for s in transposed[0]]
+    #     actions = [a for a in transposed[1]]
+    #     rewards = [r for r in transposed[2]]
+    #
+    #     next_states = [ns for ns in transposed[3]]
+    #
+    #     targets = []
+    #     predicts = []
+    #
+    #     loss = torch.nn.MSELoss()
+    #     loss_acc = 0
+    #
+    #     for i in range(self.batch_size):
+    #         targets.append(rewards[i] + self.gamma * torch.max(self.q_network(next_states[i])))
+    #         predicted = self.q_network(states[i])
+    #         predicts.append(predicted[self.action_to_index[actions[i]]])
+    #         loss_acc += loss(targets[i], predicts[i])
+    #
+    #     loss = loss_acc / self.batch_size
+    #     self.losses[self.env.now] = loss
+    #
+    #     # expected_batch = self.q_network(next_state_batch).max(1)[0]
+    #     # target = reward_batch + self.gamma * expected_batch
+    #     # state_action_values = self.q_network(state_batch).gather(1, action_batch)
+    #
+    #     return loss
+
     def _calculate_batch_loss(self, transitions):
+        # transition — (s, a, r, s')
+
         transposed = list(zip(*transitions))
 
-        states = [s for s in transposed[0]]
-        actions = [a for a in transposed[1]]
-        rewards = [r for r in transposed[2]]
+        states = torch.cat([s.unsqueeze(0) for s in transposed[0]])
+        actions = torch.cat([torch.tensor([self.action_to_index[a]], dtype=torch.int64).unsqueeze(0) for a in transposed[1]])
+        rewards = torch.cat([torch.tensor([r]).unsqueeze(0) for r in transposed[2]])
 
-        next_states = [ns for ns in transposed[3]]
+        next_states = torch.cat([ns.unsqueeze(0) for ns in transposed[3]])
 
-        targets = []
-        predicts = []
+        if self.sarsa:
+            ### TODO This line is too complicated, need to implement choose_next_action for batch input ###
+            next_actions = torch.cat([torch.tensor([self.action_to_index[self.choose_next_action(ns)]], dtype=torch.int64).unsqueeze(0) for ns in transposed[3]])
+            target = rewards + self.gamma * self.q_network(next_states).gather(1, next_actions)
+        else:
+            ### the same operation broken down ###
+            # temp = self.q_network(next_states)
+            # temp = torch.max(temp, dim=1)[0]
+            # temp = temp.detach()
+            # temp = temp.squeeze(0).view(self.batch_size, 1)
+            temp = torch.max(self.q_network(next_states), dim=1)[0].detach().squeeze(0).view(self.batch_size, 1)
+            target = rewards + temp * self.gamma
+
+        predicted = self.q_network(states)
+        predicted = predicted.gather(1, actions)
 
         loss = torch.nn.MSELoss()
-        loss_acc = 0
-
-        for i in range(self.batch_size):
-            targets.append(rewards[i] + self.gamma * torch.max(self.q_network(next_states[i])))
-            predicted = self.q_network(states[i])
-            predicts.append(predicted[self.action_to_index[actions[i]]])
-            loss_acc += loss(targets[i], predicts[i])
-
-        loss = loss_acc / self.batch_size
+        loss = loss(target, predicted)
+        # loss_acc = 0
+        #
+        # for i in range(self.batch_size):
+        #     targets.append(rewards[i] + self.gamma * torch.max(self.q_network(next_states[i])))
+        #     predicted = self.q_network(states[i])
+        #     predicts.append(predicted[self.action_to_index[actions[i]]])
+        #     loss_acc += loss(targets[i], predicts[i])
+        #
+        # loss = loss_acc / self.batch_size
         self.losses[self.env.now] = loss
 
         # expected_batch = self.q_network(next_state_batch).max(1)[0]
@@ -251,6 +304,7 @@ class DeepLearningAgent:
         # state_action_values = self.q_network(state_batch).gather(1, action_batch)
 
         return loss
+
 
     # Function to calculate the loss for a particular transition.
     # transition = (s, a, r, s')
@@ -280,7 +334,7 @@ class Network(torch.nn.Module):
         layers.append(torch.nn.Linear(in_features=input_dimensions, out_features=100))
         layers.append(torch.nn.ReLU())
         for i in range(depth-1):
-            layers.append(torch.nn.Linear(in_features=100, out_features=100))
+            layers.append(weight_norm(torch.nn.Linear(in_features=100, out_features=100), name='weight'))
             layers.append(torch.nn.ReLU())
         layers.append(torch.nn.Linear(in_features=100, out_features=output_dimensions))
 
